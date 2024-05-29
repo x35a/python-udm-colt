2db1309edfd18e74321e56d9925105beef9cd9e3
xj <8172473+x35a@users.noreply.github.com>
2024-05-29 09:30:08 +0000

    up

--- main.py
+++ main.py
@@ -11,6 +11,9 @@
 
 # type() # check type
 
+# multiple variable assignment
+# all, at, once = 5, 10, 15
+
 # Numbers
 # division always returns a floating point number
 # print(5/1) # 5.0
@@ -114,18 +117,84 @@
 # https://www.udemy.com/course/the-modern-python3-bootcamp/learn/lecture/7991014#overview
 
 # Lists
+# vid 97-110
 # Lists are mutable sequences, typically used to store collections of homogeneous items.
 # https://docs.python.org/3.12/library/stdtypes.html#lists
 # Common Sequence Operations https://docs.python.org/3.12/library/stdtypes.html#common-sequence-operations
 # Mutable Sequence Operations https://docs.python.org/3.12/library/stdtypes.html#mutable-sequence-types
 
 # len([a,b,c]) # 3 # returns list length
-# len(range(3)) # 3 # returns range 
+# len(range(3)) # 3 # returns range
 # list(range(1, 100)) # list of numbers from 1 to 99
 # somelist[index] # accesses list item at index
 # somelist[-1] # negative number to index backwards
 # 2.50 https://www.udemy.com/course/the-modern-python3-bootcamp/learn/lecture/7991020#questions
-# Check if a value is in a list 
+# Check if a value is in a list
 # 'a' in ['a', 'b', 'c'] # True
 # https://docs.python.org/3.12/library/stdtypes.html#common-sequence-operations
 # 3.40 https://www.udemy.com/course/the-modern-python3-bootcamp/learn/lecture/7991020#questions
+
+# iterating over lists
+# vid 102
+# colors = ['red', 'green', 'blue']
+# for color in colors # loop through list
+# while loop
+
+# list methods
+# .append() # adds item to end of list
+# .extend() # adds items to end of list
+# .insert(index, item) # adds item to index of list
+# .insert(len(list), item) # adds item to end of list
+# .clear() # removes all items from list
+# .pop() # removes last item from list
+# .pop(1) # removes item at index 1
+# .remove(item) # removes item from list
+# .index(item, x, j) # returns index of item in list
+# .sort() # sorts list in place
+# https://docs.python.org/3.12/library/stdtypes.html#list.sort
+# list to string using string method .join()
+# ','.join(['a', 'b', 'c']) # a,b,c
+
+# list slicing
+# newlist = list[0:] # copy the list (probably shallow)
+# slicing doesn't change the original list
+# 4.00 https://www.udemy.com/course/the-modern-python3-bootcamp/learn/lecture/7991022?start=15#questions
+
+# flist = ['a', 'b', 'c', 'd']
+# slist = flist
+# tlist = flist[0:]
+# print(slist is flist) # true
+# print(tlist is flist) # fasle
+
+# s[i:j] # returns list from index i to j-1
+# s[i:j:k] # returns list from index i to j-1 with step k
+# s[i:] # returns list from index i to end
+# s[:j] # returns list from start to j-1]
+
+# print(flist[1:3]) # b, c
+# print(flist[:3]) # a, b, c
+# print(flist[1:]) # b, c, d
+# print(flist[1:4:2]) # b, d
+# print(flist[10:20]) # []
+# print(flist[10:]) # []
+# print(flist[-1]) # d
+# print(flist[-3:]) # b, c, d
+# print(flist[-3:-1]) # b, c
+# print(flist[-4:-1:2]) # a, c
+# print(flist[-10:]) # a, b, c, d
+# print(flist[-10:-7]) # []
+# print(flist[:-10]) # []
+# print(flist[::-1]) # d, c, b, a # -1 step means reverse
+# print(flist[0:3:-1])  # []
+# print(flist[2:0:-1]) # c, b
+# print(flist[1::-1])  # b, a
+# print(flist[:1:-1])  # d, c
+
+# modify portions of list
+# numbers = [1, 2, 3, 4, 5]
+# numbers[1:3] = [10, 20] # replaces 2 and 3 with 10, 20
+
+# swappin values
+# https://www.udemy.com/course/the-modern-python3-bootcamp/learn/lecture/8845920#overview
+# ls = ['a', 'b']
+# ls[0], ls[1] = ls[1], ls[0] # [b, a]]
